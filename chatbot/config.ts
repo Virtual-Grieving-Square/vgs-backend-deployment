1d5303e221e9acb9b4f070e9a850cb9e7bcb1a02
nattyengeda <nattynengeda@gmail.com>
2024-04-03 15:45:57 +0300

    Delete ActionProvider, MessageParser, and config files

--- chatbot/config.ts
+++
@@ -1,7 +0,0 @@
-import { createChatBotMessage } from 'react-chatbot-kit';
-
-const config = {
-  initialMessages: [createChatBotMessage(`Hello, How can we help you today?`, {})],
-};
-
-export default config;
