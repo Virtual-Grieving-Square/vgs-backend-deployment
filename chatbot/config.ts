f0e47f1b4033a1e47c7e06ffe77f52ef8830313c
nattyengeda <nattynengeda@gmail.com>
2024-04-03 15:43:48 +0300

    Update function to handle edge cases

---
+++ chatbot/config.ts
@@ -0,0 +1,7 @@
+import { createChatBotMessage } from 'react-chatbot-kit';
+
+const config = {
+  initialMessages: [createChatBotMessage(`Hello, How can we help you today?`, {})],
+};
+
+export default config;
