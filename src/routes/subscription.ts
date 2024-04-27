2eb694797d10f0fceae24728e1d69f2cca651ada
nattyengeda <nattynengeda@gmail.com>
2024-04-27 12:54:12 +0300

    Refactor subscription controller and routes

--- src/routes/subscription.ts
+++ src/routes/subscription.ts
@@ -1,16 +1,10 @@
 import express from 'express';
 
 // Controllers
-import {
-  addSubscription,
-  handleStripeWebhook,
-  purchaseSubscription,
-} from '../controller/subscription';
+
 
 const router = express.Router();
 
-router.post("/addSubscription", addSubscription);
-router.post('/create-checkout-session/:local', purchaseSubscription);
-router.post("/webhooks/stripe", handleStripeWebhook);
+
 
 export default router; 
