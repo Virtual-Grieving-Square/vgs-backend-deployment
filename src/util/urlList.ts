21b6bf9b1bec2945561d2c205e9dcd6deb5ab7cc
natnaelengeda <nattynengeda@gmail.com>
2024-05-08 23:21:39 +0300

    chore: Update URL list in urlList.ts

--- src/util/urlList.ts
+++ src/util/urlList.ts
@@ -1,10 +1,15 @@
 export const urlList = [
+  // Local
   "http://localhost:5173",
   "http://localhost:3131",
   "http://localhost:3132",
-  "https://uione.virtualgrievingsquare.com",
   "http://192.168.0.108:3131",
+
+
+  // Production
+  "https://uione.virtualgrievingsquare.com",
   "https://5729-196-188-123-14.ngrok-free.app",
   "https://deoow05yxfsr.cloudfront.net/",
-  "https://virtualgrievingsquare.com"
+  "https://virtualgrievingsquare.com",
+  "https://admin.virtualgrievingsquare.com/",
 ];
