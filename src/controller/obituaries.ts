3ed1cabcb7a416a38187fa6c72e670c1bc059bf5
natnaelengeda <nattynengeda@gmail.com>
2024-05-19 11:28:11 +0300

    Refactor getRecentObituaries endpoint to support pagination

--- src/controller/obituaries.ts
+++ src/controller/obituaries.ts
@@ -5,10 +5,13 @@ import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "../util/awsAccess";
 import { Stream } from "stream";
 
-export const getRecentObituaries = async (req: Request, res: Response) => {
+export const getRecentObituaries = async (req: any, res: Response) => {
   try {
     const sixMonthsAgo = new Date();
     sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
 
     const humanObituaries = await HumanMemorial.aggregate([
       {
@@ -28,7 +31,16 @@ export const getRecentObituaries = async (req: Request, res: Response) => {
 
     const recentObituaries = [...humanObituaries, ...famousObituaries];
 
-    res.status(200).json(recentObituaries);
+    const total = recentObituaries.length;
+
+    res.status(200).json({
+      total: total,
+      page: page,
+      limit: limit,
+      totalPages: Math.ceil(total / limit),
+      obituaries: recentObituaries.slice(skip, skip + limit),
+    });
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
