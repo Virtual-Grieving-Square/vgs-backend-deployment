fa7d8e5e08b348fc6052f5af2c5b46a22756d93b
Natnael Engeda <nattynengeda@gmail.com>
2024-06-05 11:59:39 +0300

    chore: Update emailSubscribe controller to include memorials in subscribeEmail endpoint

--- src/controller/email.ts
+++ src/controller/email.ts
@@ -3,19 +3,23 @@ import { EmailSubscribe } from '../model/emailSubscribe';
 
 export const subscribeEmail = async (req: Request, res: Response) => {
   try {
-    const { email } = req.body;
+    const { email, memorials } = req.body;
 
     if (!email) {
       return res.status(400).json({ message: "Email is required" });
     }
     // Check if email is already subscribed
     const isSubscribed = await EmailSubscribe.findOne({ email });
+
     if (isSubscribed) {
-      return res.status(400).json({ message: "Email is already subscribed" });
+      return res.status(403).json({ message: "Email is already subscribed" });
     }
 
-    // Subscribe email
-    const newEmail = new EmailSubscribe({ email });
+    // // Subscribe email
+    const newEmail = new EmailSubscribe({
+      email: email,
+      memorials: memorials
+    });
 
     await newEmail.save();
     res.status(200).json({ message: "Email subscribed successfully" });
--- src/model/emailSubscribe.ts
+++ src/model/emailSubscribe.ts
@@ -1,15 +1,20 @@
 import mongoose, { Schema, Document } from "mongoose";
 
+export interface EmailSubscribe extends Document {
+  email: string;
+  memorials: string[];
+}
+
 const emailSubscribeSchema: Schema = new Schema({
   email: { type: String, required: true, unique: true },
+  memorials: [{ type: Schema.Types.ObjectId, ref: "Memorial", default: [] }],
+
 
   //Timestamp
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
-export interface EmailSubscribe extends Document {
-  email: string;
-}
+
 
 export const EmailSubscribe = mongoose.model("EmailSubscribe", emailSubscribeSchema);
