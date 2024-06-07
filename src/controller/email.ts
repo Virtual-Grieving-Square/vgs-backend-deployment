904ba5de88a034e29f84482be931c966087a8d06
Natnael Engeda <nattynengeda@gmail.com>
2024-06-07 19:42:55 +0300

    chore: Update emailSubscribe controller to include memorials in subscribeEmail endpoint

--- src/controller/email.ts
+++ src/controller/email.ts
@@ -1,6 +1,12 @@
 import { Response, Request } from 'express';
 import { EmailSubscribe } from '../model/emailSubscribe';
 
+import path from "path";
+import fs from "fs";
+import ejs from "ejs";
+
+import nodemailer from "nodemailer";
+
 export const subscribeEmail = async (req: Request, res: Response) => {
   try {
     const { email, memorials } = req.body;
@@ -23,6 +29,38 @@ export const subscribeEmail = async (req: Request, res: Response) => {
 
     await newEmail.save();
     res.status(200).json({ message: "Email subscribed successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export const Test = async (req: Request, res: Response) => {
+  try {
+    const env = process.env;
+
+    var ejsTemplatePath = path.join(__dirname!, '../../src/pages/auth/index.html');
+    const ejsTemplate = fs.readFileSync(ejsTemplatePath, "utf-8");
+    const renderHtml = ejs.render(ejsTemplate, { name: `${"Natnael"} ${"Engeda"}`, code: 123123 });
+
+    const transporter = nodemailer.createTransport({
+      host: env.NODEMAILER_HOST!,
+      port: 465,
+      secure: true,
+      auth: {
+        user: env.NODEMAILER_USER!,
+        pass: env.NODEMAILER_PASS!,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Virtual Grieving Square" <verification@virtualgrievingsquare.com>',
+      to: "nattynengeda@gmail.com",
+      subject: "Virtual Grieving Square Verification",
+      html: renderHtml,
+    });
+
+    return res.status(200).json({ message: "Email sent successfully", info: info });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
