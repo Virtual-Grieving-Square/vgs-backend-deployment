b47759a110a283defcce334b9458897ceff73151
Aklilegetachew <aklilegetachew@gmail.com>
2024-07-11 21:12:45 +0300

    io

--- src/util/event.ts
+++ src/util/event.ts
@@ -1,13 +1,84 @@
-// import { io } from "../index";
-// import { UserModel } from "../model/user";
+import { io } from "../index";
+import NotifModel, { INotif } from "../model/notification";
+import { UserModel, User } from "../model/user";
 
-// export const emitLikeUpdate = (userId: any, data: any) => {
-//   io.emit("server_update_like", data);
-// };
+interface DataToSend {
+  user: string;
+  userID: string;
+  msg: string;
+  type: "like" | "comment";
+}
 
-// export const emitcomment = (userId: any, data: any) => {
-//   const user = UserModel.findById(userId);
-//   if (!user) {
-//   }
-//   io.emit(user?.socketId, data);
-// };
+// Helper function to construct the data to send
+const constructDataToSend = (
+  user: User,
+  message: string,
+  type: "like" | "comment"
+): DataToSend => {
+  return {
+    user: `${user.firstName} ${user.lastName}`,
+    userID: user._id.toString(),
+    msg: message,
+    type: type,
+  };
+};
+
+const saveNotification = async (
+  userId: string,
+  message: string,
+  type: "like" | "comment"
+): Promise<void> => {
+  try {
+    const notification: Partial<INotif> = {
+      userID: userId,
+      Note: message,
+      seen: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    await NotifModel.create(notification);
+  } catch (error) {
+    console.error("Error saving notification:", error);
+  }
+};
+
+// Emit like update
+export const emitLikeUpdate = async (
+  userId: any,
+  data: string
+): Promise<void> => {
+  try {
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      console.error("User not found");
+      return;
+    }
+
+    const dataToSend = constructDataToSend(user, data, "like");
+    io.to(user.socketId).emit("server_update_like", dataToSend);
+    await saveNotification(userId, data, 'like');
+  } catch (error) {
+    console.error("Error fetching user:", error);
+  }
+};
+
+// Emit comment update
+export const emitCommentUpdate = async (
+  userId: any,
+  data: string
+): Promise<void> => {
+  try {
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      console.error("User not found");
+      return;
+    }
+
+    const dataToSend = constructDataToSend(user, data, "comment");
+    io.to(user.socketId).emit("server_update_comment", dataToSend);
+    await saveNotification(userId, data, 'comment');
+  } catch (error) {
+    console.error("Error fetching user:", error);
+  }
+};
