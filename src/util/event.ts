55b52692f622fa2752994bab3394192888e79abc
Aklilegetachew <aklilegetachew@gmail.com>
2024-07-11 19:30:14 +0300

    push fcm regestration8

---
+++ src/util/event.ts
@@ -0,0 +1,13 @@
+// import { io } from "../index";
+// import { UserModel } from "../model/user";
+
+// export const emitLikeUpdate = (userId: any, data: any) => {
+//   io.emit("server_update_like", data);
+// };
+
+// export const emitcomment = (userId: any, data: any) => {
+//   const user = UserModel.findById(userId);
+//   if (!user) {
+//   }
+//   io.emit(user?.socketId, data);
+// };
