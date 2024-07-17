206b0a38888a932fcbb46a3862f52f0d7126e901
Aklilegetachew <aklilegetachew@gmail.com>
2024-07-17 22:29:00 +0300

    self notification

--- src/controller/humanMemorial.ts
+++ src/controller/humanMemorial.ts
@@ -464,7 +464,7 @@ export const createMemorialComment = async (req: Request, res: Response) => {
 
           for (const tokenData of authorTokens) {
             const payload = {
-              title: "Your comment got new comment!",
+              title: "Your Memorial got new comment!",
               body: `${user?.firstName} ${user?.lastName} commented on your memorial.`,
               data: {},
             };
