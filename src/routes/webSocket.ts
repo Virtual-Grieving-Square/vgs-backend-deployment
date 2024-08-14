f36348973b274bef00f26552cc01b2dfecf175ee
Aklilegetachew <aklilegetachew@gmail.com>
2024-08-14 09:56:30 +0300

    check the dev

--- src/routes/webSocket.ts
+++ src/routes/webSocket.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { Connect, Disconnect } from "../controller/webSockets";
+import { Connect, Disconnect, Defualt } from "../controller/webSockets";
 
 const router = express.Router();
 
 router.post("/connection", Connect);
+router.post("/defualt", Defualt);
 router.post("/disconnect", Disconnect);
 
 export default router;
