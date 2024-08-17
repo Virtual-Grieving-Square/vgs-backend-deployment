f1ce7a42cdfa5213be718734738e01be58ee9967
Aklilegetachew <aklilegetachew@gmail.com>
2024-08-17 20:57:54 +0300

    options

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,7 @@ app.use(
     origin: urlList,
     optionsSuccessStatus: 200,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
